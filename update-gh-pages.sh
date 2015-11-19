9bf18ccff1377012683e2a65fc4e46c95df13e41
David Barri <japgolly@gmail.com>
2015-11-20 00:53:39 +1100

    Add gh-pages

---
+++ update-gh-pages.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+cd "$(dirname "$0")" || exit 1
+
+depS=demo/target/scala-2.11/demo-jsdeps.min.js
+outS=demo/target/output.js
+
+depT=res/jsdeps.js
+outT=res/main.js
+deps="$depT $outT"
+
+rm -f $deps
+
+sbt clean demo/fullOptJS \
+&& echo \
+&& cp -v $depS $depT \
+&& cp -v $outS $outT \
+&& git add $deps \
+&& git st \
+&& echo "git commit -m 'Refresh gh-pages'" && echo
+
