d56280e589786dc9cc15376ee680c18fdf5e5303
David Barri <japgolly@gmail.com>
2015-11-23 09:10:53 +1100

    Add BuildSet BM

--- demo/src/main/scala/demo/suites/scala/package.scala
+++ demo/src/main/scala/demo/suites/scala/package.scala
@@ -5,6 +5,7 @@ import japgolly.scalajs.benchmark.gui.MenuComp
 package object scala {
 
   lazy val all = MenuComp.folder("Scala")(
+    BuildSet.guiSuite,
     IntSet.guiSuite,
     VectorIndex.guiSuite)
 }
