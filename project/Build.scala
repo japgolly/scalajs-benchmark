38f2d327880347c09eb743d39373d11087f19db1
David Barri <japgolly@gmail.com>
2015-12-05 23:46:11 +1100

    Release v0.2.1

--- project/Build.scala
+++ project/Build.scala
@@ -25,7 +25,7 @@ object ScalaJsBenchmark extends Build {
   val commonSettings: PE =
     _.settings(
       organization             := "com.github.japgolly.scalajs-benchmark",
-      version                  := "0.2.1-SNAPSHOT",
+      version                  := "0.2.1",
       homepage                 := Some(url("https://github.com/japgolly/" + ghProject)),
       licenses                 += ("Apache-2.0", url("http://opensource.org/licenses/Apache-2.0")),
       scalaVersion             := Ver.Scala211,
