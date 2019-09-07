7055960f19779c9e30acc5d1917b29abf989f0c7
David Barri <japgolly@gmail.com>
2019-09-07 12:13:05 +1000

    Support Scala 2.13

--- update-gh-pages.sh
+++ update-gh-pages.sh
@@ -3,13 +3,14 @@ cd "$(dirname "$0")" || exit 1
 
 set -e
 
-[ -e res ] && rm -rf res
-mkdir res
+git checkout res
+rm -rf res/scala-2.1{2,3}
+rm -rf res/scala21{2,3}-*.html
 cp -v demo/*.html res
 sed -e 's!target/!!' -i res/*.html
 sbt +clean +demo/fastOptJS +demo/fullOptJS
 echo
-for v in 2.11 2.12; do mkdir res/scala-$v; cp -v demo/target/scala-$v/demo-* res/scala-$v; done
+for v in 2.12 2.13; do mkdir res/scala-$v; cp -v demo/target/scala-$v/demo-* res/scala-$v; done
 git add res
 git st
 echo "git commit -m 'Refresh gh-pages' && git push && git checkout master" && echo
