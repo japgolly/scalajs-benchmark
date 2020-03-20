96b7910d22abb5bd88d48dd8f5a21b0030956790
David Barri <japgolly@gmail.com>
2020-03-20 11:05:41 +1100

    Support more versions of stuff

--- update-gh-pages.sh
+++ update-gh-pages.sh
@@ -3,11 +3,11 @@ cd "$(dirname "$0")" || exit 1
 
 set -e
 
-git checkout res
+# git checkout res
 rm -rf res/scala-2.1{2,3}
-rm -rf res/scala21{2,3}-*.html
-cp -v demo/*.html res
-sed -e 's!target/!!' -i res/*.html
+# rm -rf res/scala21{2,3}-*.html
+# cp -v demo/*.html res
+# sed -e 's!target/!!' -i res/*.html
 sbt +clean +demo/fastOptJS +demo/fullOptJS
 echo
 for v in 2.12 2.13; do mkdir res/scala-$v; cp -v demo/target/scala-$v/demo-* res/scala-$v; done
