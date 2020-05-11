e201b98ae23b514a9a219bccb078ddb619182418
Scala Steward <me@scala-steward.org>
2020-05-11 13:04:09 +0200

    Update scalacss:core, scalacss:ext-react to 0.6.1

--- project/Build.scala
+++ project/Build.scala
@@ -19,7 +19,7 @@ object ScalaJsBenchmark {
     val Scala212         = "2.12.11"
     val Scala213         = "2.13.2"
     val ScalaCollCompat  = "2.1.6"
-    val ScalaCss         = "0.6.0"
+    val ScalaCss         = "0.6.1"
     val ScalaJsReact     = "1.6.0"
     val Scalaz           = "7.2.30"
 
