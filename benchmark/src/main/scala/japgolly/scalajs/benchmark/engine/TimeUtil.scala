18226cbf0c011b677c85be96b8e1425c691147c0
David Barri <japgolly@gmail.com>
2020-05-19 15:44:51 +1000

    Fix TimeUtil.toMs(NaN)

--- benchmark/src/main/scala/japgolly/scalajs/benchmark/engine/TimeUtil.scala
+++ benchmark/src/main/scala/japgolly/scalajs/benchmark/engine/TimeUtil.scala
@@ -8,11 +8,15 @@ import scala.scalajs.js
 object TimeUtil {
 
   def toMs(d: Duration): Double = {
-    d.unit match {
-      case TimeUnit.NANOSECONDS  => d.length.toDouble / 1000000.0
-      case TimeUnit.MICROSECONDS => d.length.toDouble / 1000.0
-      case TimeUnit.MILLISECONDS => d.length.toDouble
-      case _                     => d.toMillis.toDouble
+    d match {
+      case f: FiniteDuration =>
+        f.unit match {
+          case TimeUnit.NANOSECONDS  => d.length.toDouble / 1000000.0
+          case TimeUnit.MICROSECONDS => d.length.toDouble / 1000.0
+          case TimeUnit.MILLISECONDS => d.length.toDouble
+          case _                     => d.toMillis.toDouble
+        }
+      case _ => Double.NaN
     }
   }
 
