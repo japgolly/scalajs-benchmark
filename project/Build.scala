0907ee565868a27cf0855a7eb9cab857c26089bb
Scala Steward <me@scala-steward.org>
2021-05-28 12:24:08 +0200

    Update scala-compiler, scala-library to 2.12.14

--- project/Build.scala
+++ project/Build.scala
@@ -22,7 +22,7 @@ object ScalaJsBenchmark {
     val Microlibs        = "2.5"
     val Monocle          = "1.6.3"
     val React            = "16.14.0"
-    val Scala212         = "2.12.13"
+    val Scala212         = "2.12.14"
     val Scala213         = "2.13.4"
     val ScalaCollCompat  = "2.3.2"
     val ScalaCss         = "0.7.0"
