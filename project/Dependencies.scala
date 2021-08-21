cac4a21bb63eb08d273d66f25672f53056116ff1
Scala Steward <me@scala-steward.org>
2021-08-21 08:22:01 +0200

    Update scalaz-core, scalaz-effect to 7.3.5

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -28,7 +28,7 @@ object Dependencies {
     // Demo only
     val catsEffect      = "3.2.2"
     val scalaJsJavaTime = "2.3.0"
-    val scalaz          = "7.3.4"
+    val scalaz          = "7.3.5"
     val shapeless       = "2.3.7"
     val zio             = "1.0.10"
   }
