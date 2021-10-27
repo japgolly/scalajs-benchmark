c98b424999fb6554fe8d90e858efa48a700f7e78
Scala Steward <me@scala-steward.org>
2021-10-27 08:46:32 +0200

    Update scalajs-react:core, ... to 2.0.0-RC5

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -19,7 +19,7 @@ object Dependencies {
     val scala2       = "2.13.6"
     val scala3       = "3.0.1"
     val scalaCss     = "1.0.0-RC2"
-    val scalaJsReact = "2.0.0-RC4"
+    val scalaJsReact = "2.0.0-RC5"
     val sourceCode   = "0.2.7"
 
     // Test only
